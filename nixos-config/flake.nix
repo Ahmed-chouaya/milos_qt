a624060eb03b0074c0adab9dd75afe2dd84e984c
Ahmed Chouaya <chouaya.ahmed83@gmail.com>
2026-01-03 15:55:20 +0100

    fix: correct GitHub username case in flake.nix milos-qt URL
    Change from ahmedchouaya to Ahmed-chouaya for consistency with repository name

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs = nixpkgs;
-    milos-qt.url = "github:ahmedchouaya/milos-qt";
+    milos-qt.url = "github:Ahmed-chouaya/milos-qt";
     milos-qt.inputs.nixpkgs = nixpkgs;
   };
 
